8fe41a093ed71cc66212e19866f5f99919c9fab5
vanpipy <vanpipy@gmail.com>
2025-05-25 23:05:26 +0800

    chore: update deps and add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+    .setTitle('Islands')
+    .setDescription('The Islands API description')
+    .setVersion('1.0')
+    .addTag('islands')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
